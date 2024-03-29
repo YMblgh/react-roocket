0e3e517daf3a13ddf899747a3c31e08cc98e390e
YounesMoballegh <ymblgh@outlook.com>
2024-03-29 13:12:36 +0330

    a bug fixed.

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -10,7 +10,7 @@ export default ({ data }) => {
     return (
         <Link className="bg-white rounded-3xl shadow-md p-4 delay-75 duration-500 sm:hover:-translate-y-2 sm:hover:scale-105 space-y-5 flex flex-col justify-between">
             <div className="space-y-3">
-            <img className="w-full aspect-video rounded-2xl object-cover shadow-xl shadow-slate-700" src={data.postImg} alt="" />
+            <img className="w-full aspect-video rounded-2xl object-cover shadow-xl shadow-slate-700 bg-slate-700" src={data.postImg} alt="" />
             <h3 className="font-semibold text-xl text-ellipsis line-clamp-2">{data.title}</h3>
             <p className="text-slate-400 line-clamp-3">{data.desc}</p>
             </div>
